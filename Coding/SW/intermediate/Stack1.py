22f02ff60fe0c4b81953bcb5f6cff8d162f7dd70
JuhoSon <zoohoson@gmail.com>
2021-07-17 23:47:59 +0900

    4871

--- Coding/SW/intermediate/Stack1.py
+++ Coding/SW/intermediate/Stack1.py
@@ -107,6 +107,30 @@ T = int(input())
 
 V, E = list(map(int, input().split(' ')))
 
+# 인터넷
+
+
+def dfs(node_index, visited, nodes):
+    visited[node_index] = 1
+    for node in nodes[node_index]:
+        if visited[node] != 1:
+            dfs(node, visited, nodes)
+
+
+for t in range(int(input())):
+    V, E = map(int, input().split())
+    nodes = [[] for _ in range(V+1)]
+    for _ in range(E):
+        start, end = map(int, input().split())
+        nodes[start].append(end)
+    S, G = map(int, input().split())
+    visited = [0 for _ in range(V+1)]
+    dfs(S, visited, nodes)
+    answer = 0
+    if visited[G] == 1:
+        answer = 1
+    print('#{} {}'.format(t+1, answer))
+
 
 # '''4873 반복문자 지우기
 # 문자열 s에서 반복된 문자를 지우려고 한다. 지워진 부분은 다시 앞뒤를 연결하는데, 만약 연결에 의해 또 반복문자가 생기면 이부분을 다시 지운다.
